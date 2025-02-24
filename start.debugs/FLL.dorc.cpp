aef61ae1e4ed58534caf50403843e2c8d684ec9c
macro-ahk <mikestartpsx@gmail.com>
2025-02-24 18:33:06 +0300

    Add files via upload

---
+++ start.debugs/FLL.dorc.cpp
@@ -0,0 +1,157 @@
+
+C/C++ Runner
+Dll
+macro-ahk
+5,617,163
+
+local use() start = .(F1)
+
+local use() Stop = .(F2)
+local players.script.use
+	when joined.start++
+c#+
+local use () ~~~ 0 in 100 numbers) ~~~
+start debug F%  (33%) enchanted
+hack. local.wifi.debugger
+	file:///C:/Users/desktop/AppData/Local/
+https:local.internetshare/
+	open/019++() 
+
+ahk/click 000% 142(Z) 00(x) y(00)
+	click 000% 142(Z) 00(x) y(00)
+020% 142(Z) 09(x) y(750)
+	200% 142(Z) 00(x) y(00)
+010% 142(Z) 00(x) y(013)
+	000% 149(Z) 0110(x) y(020)
+RUN/AS/adminastor/
+//https/.request/local/internet/location.steal
+000% 142(Z) 00(x) y(00)
+000% 142(Z) 00(x) y(00)
+000% 142(Z) 00(x) y(00)
+000% 142(Z) 00(x) y(00)
+https:local.internetshare/
+https:local.internetshare/
+/steal
+/local
+file:///C:/Users/desktop/AppData/Local/
+file:///C:/Users/desktop/AppData/Local/
+file:///C:/Users/desktop/AppData/Local/
+file:///C:/Users/desktop/AppData/Local/
+	
+C/C++ Runner
+Dll
+macro-ahk
+5,617,163
+
+local use() start = .(F1)
+
+local use() Stop = .(F2)
+local players.script.use
+	when joined.start++
+c#+
+local use () ~~~ 0 in 100 numbers) ~~~
+start debug F%  (33%) enchanted
+hack. local.wifi.debugger
+	file:///C:/Users/desktop/AppData/Local/
+https:local.internetshare/
+	open/019++() 
+
+ahk/click 000% 142(Z) 00(x) y(00)
+	click 000% 142(Z) 00(x) y(00)
+020% 142(Z) 09(x) y(750)
+	200% 142(Z) 00(x) y(00)
+010% 142(Z) 00(x) y(013)
+	000% 149(Z) 0110(x) y(020)
+RUN/AS/adminastor/
+//https/.request/local/internet/location.steal
+000% 142(Z) 00(x) y(00)
+000% 142(Z) 00(x) y(00)
+000% 142(Z) 00(x) y(00)
+000% 142(Z) 00(x) y(00)
+https:local.internetshare/
+https:local.internetshare/
+/steal
+/local
+file:///C:/Users/desktop/AppData/Local/
+file:///C:/Users/desktop/AppData/Local/
+file:///C:/Users/desktop/AppData/Local/
+file:///C:/Users/desktop/AppData/Local/
+	
+C/C++ Runner
+Dll
+macro-ahk
+5,617,163
+
+local use() start = .(F1)
+
+local use() Stop = .(F2)
+local players.script.use
+	when joined.start++
+c#+
+local use () ~~~ 0 in 100 numbers) ~~~
+start debug F%  (33%) enchanted
+hack. local.wifi.debugger
+	file:///C:/Users/desktop/AppData/Local/
+https:local.internetshare/
+	open/019++() 
+
+ahk/click 000% 142(Z) 00(x) y(00)
+	click 000% 142(Z) 00(x) y(00)
+020% 142(Z) 09(x) y(750)
+	200% 142(Z) 00(x) y(00)
+010% 142(Z) 00(x) y(013)
+	000% 149(Z) 0110(x) y(020)
+RUN/AS/adminastor/
+//https/.request/local/internet/location.steal
+000% 142(Z) 00(x) y(00)
+000% 142(Z) 00(x) y(00)
+000% 142(Z) 00(x) y(00)
+000% 142(Z) 00(x) y(00)
+https:local.internetshare/
+https:local.internetshare/
+/steal
+/local
+file:///C:/Users/desktop/AppData/Local/
+file:///C:/Users/desktop/AppData/Local/
+file:///C:/Users/desktop/AppData/Local/
+file:///C:/Users/desktop/AppData/Local/
+	
+C/C++ Runner
+Dll
+macro-ahk
+5,617,163
+
+local use() start = .(F1)
+
+local use() Stop = .(F2)
+local players.script.use
+	when joined.start++
+c#+
+local use () ~~~ 0 in 100 numbers) ~~~
+start debug F%  (33%) enchanted
+hack. local.wifi.debugger
+	file:///C:/Users/desktop/AppData/Local/
+https:local.internetshare/
+	open/019++() 
+
+ahk/click 000% 142(Z) 00(x) y(00)
+	click 000% 142(Z) 00(x) y(00)
+020% 142(Z) 09(x) y(750)
+	200% 142(Z) 00(x) y(00)
+010% 142(Z) 00(x) y(013)
+	000% 149(Z) 0110(x) y(020)
+RUN/AS/adminastor/
+//https/.request/local/internet/location.steal
+000% 142(Z) 00(x) y(00)
+000% 142(Z) 00(x) y(00)
+000% 142(Z) 00(x) y(00)
+000% 142(Z) 00(x) y(00)
+https:local.internetshare/
+https:local.internetshare/
+/steal
+/local
+file:///C:/Users/desktop/AppData/Local/
+file:///C:/Users/desktop/AppData/Local/
+file:///C:/Users/desktop/AppData/Local/
+file:///C:/Users/desktop/AppData/Local/
+	
